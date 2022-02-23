b565e37dfb7ee1401996f41277ddc4b6ed6a43a6
ixtiyor21 <ixtiyorqozoqboyev@gmail.com>
2022-02-23 22:41:37 +0500

    column controller removed

--- src/main/java/uz/boom/citizens/controller/column/ColumnController.java
+++ src/main/java/uz/boom/citizens/controller/column/ColumnController.java
@@ -22,15 +22,11 @@ public class ColumnController extends AbstractController<ColumnService> {
     }
 
     @RequestMapping(value = "create", method = RequestMethod.GET)
-    public String columnPage(){
+    public String columnPage()
+    {
         return "column/create";
     }
 
-    @RequestMapping(value = "create",method = RequestMethod.POST)
-    public String create(@ModelAttribute ColumnCreateDto dto) throws IOException {
-        service.create(dto);
-        return "redirect:/";
-    }
 
     @RequestMapping(value = "create",method = RequestMethod.POST)
     public String test(@ModelAttribute ColumnCreateDto dto) throws IOException {
