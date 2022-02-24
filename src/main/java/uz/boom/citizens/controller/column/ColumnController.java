2953b4384cd2945c85128eaf50179dadd2ea688c
mreed <mr.eed7420@gmail.com>
2022-02-24 09:03:14 +0500

    controller cruds

--- src/main/java/uz/boom/citizens/controller/column/ColumnController.java
+++ src/main/java/uz/boom/citizens/controller/column/ColumnController.java
@@ -25,44 +25,49 @@ public class ColumnController extends AbstractController<ColumnService> {
     }
 
     @RequestMapping(value = "create", method = RequestMethod.GET)
-    public String columnPage()
-    {
+    public String columnPage() {
         return "column/create";
     }
 
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    public String create(@ModelAttribute ColumnCreateDto dto) throws IOException {
+        service.create(dto);
+        return "redirect:/";
+    }
 
 
-    @RequestMapping(value = "update/{id}/",method = RequestMethod.GET)
-    public String updatePage( Model model, @PathVariable(name = "id") Long id){
-        model.addAttribute("column",service.get(id));
+    @RequestMapping(value = "update/{id}/", method = RequestMethod.GET)
+    public String updatePage(Model model, @PathVariable(name = "id") Long id) {
+        model.addAttribute("column", service.get(id));
         return "column/update";
     }
 
-    @RequestMapping(value = "update/{id}/",method = RequestMethod.POST)
+    @RequestMapping(value = "update/{id}/", method = RequestMethod.POST)
     public String update(@ModelAttribute ColumnUpdateDto dto) throws IOException {
         service.update(dto);
         return "column/update";
     }
 
-    @RequestMapping(value = "list/",method = RequestMethod.GET)
-    public String list(Model model){
-        model.addAttribute("columns",service.getAll(new GenericCriteria()));
+    @RequestMapping(value = "list/", method = RequestMethod.GET)
+    public String list(Model model) {
+        model.addAttribute("columns", service.getAll(new GenericCriteria()));
         return "column/list";
     }
 
-    @RequestMapping(value = "delete/{id}",method = RequestMethod.GET)
-    public String deletePage( Model model, @PathVariable(name = "id") Long id){
+    @RequestMapping(value = "delete/{id}", method = RequestMethod.GET)
+    public String deletePage(Model model, @PathVariable(name = "id") Long id) {
         model.addAttribute("column", service.get(id));
         return "column/delete";
     }
 
-    @RequestMapping(value = "delete/{id}",method = RequestMethod.POST)
-    public String delete(@PathVariable(name = "id") Long id){
+    @RequestMapping(value = "delete/{id}", method = RequestMethod.POST)
+    public String delete(@PathVariable(name = "id") Long id) {
         service.delete(id);
         return "redirect:/";
     }
-    @RequestMapping(value = "detail/{id}/",method = RequestMethod.GET)
-    public String detail(Model model, @PathVariable Long id){
+
+    @RequestMapping(value = "detail/{id}/", method = RequestMethod.GET)
+    public String detail(Model model, @PathVariable Long id) {
         model.addAttribute("column", service.get(id));
         return "column/detail";
     }
